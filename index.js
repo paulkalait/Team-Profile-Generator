712540cd090014d2419cc1b6c9bbc0713f5a3a4a
Your Name <kalaitzidispaul@gmail.com>
2022-03-25 19:35:36 -0400

    added html template

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 //where prompt goes
+const fs = require('fs')
+const inquirer = require('inquirer')
+
+const generateHtml = require('./utils/generatehtml')
+
+const managerQuestions = [{
+
+    type: "input",
+    name: "ID Number",
+    message: "what is your ID Number?"
+},
+{
+    type: "input",
+    name: "Name",
+    message: "what is your name?"
+},
+{
+    type:"input",
+    name: "Email",
+    message: "What is your email address?"
+},
+{
+    type: "input",
+    name: "Office",
+    message: "what is your office number?"
+}
+]
+
+const EmployeeQuestions = [{
+
+    type: "input",
+    name: "ID Number",
+    message: "what is your ID Number?"
+},
+{   
+    type: "list",
+    name: "Role",
+    message: "what is your office number?",
+    choices: ['Intern', 'Engineer']
+},
+{
+    type: "input",
+    name: "Name",
+    message: "what is your name?"
+},
+{
+    type:"input",
+    name: "Email",
+    message: "What is your email address?"
+},
+{
+    type: "input",
+    name: "Office",
+    message: "what is your office number?"
+}
+]
